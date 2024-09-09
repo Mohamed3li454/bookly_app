0253af42b7236798b76beff2d1b4a668b15b0bb6
Mohamed3li454 <mohhamed4413@gmail.com>
2024-09-09 09:01:55 +0300

    add featured list view

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
-import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/custom_appbar.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/featured_list_view_item.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -10,33 +10,25 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
+        FeaturedListViewBuilder(),
       ],
     );
   }
 }
 
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
+class FeaturedListViewBuilder extends StatelessWidget {
+  const FeaturedListViewBuilder({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
-      child: Row(
-        children: [
-          Image.asset(
-            AssetsDate.logo,
-            height: 30,
-          ),
-          const Spacer(),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              FontAwesomeIcons.magnifyingGlass,
-              size: 28,
-            ),
-          )
-        ],
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) => const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: FeaturedListViewItem(),
+        ),
       ),
     );
   }
